87c46b028c3f794252247b0a7edf8695945ffa0c
Muraree <murareelalsahu@gmail.com>
2015-01-21 14:45:50 +0530

    flash message with more clear flow

--- app/controllers/test_settings_controller.rb
+++ app/controllers/test_settings_controller.rb
@@ -6,8 +6,12 @@ class TestSettingsController < ApplicationController
   # This action is used to chek the global setting. if notification is enabled and the email is is given,
   # Then the email should be sent.
   def send_email
-    Notifier.test_message(setting.try(:email), params[:message]).deliver
-    flash[:notice] = "Message sent successfully."
+    unless params[:message].blank?
+      Notifier.test_message(current_user.setting.try(:email), params[:message]).deliver
+      flash[:notice] = "Message sent successfully."   
+    else
+      flash[:notice] = "Please insert the message"
+    end
     redirect_to new_email_test_settings_path
   end
 
@@ -19,7 +23,7 @@ class TestSettingsController < ApplicationController
   # This filter redirect to global setting page, if setting is not proper.
   def can_send_message
     unless current_user.can_send_notification?
-      redirect_to new_setting_path, :notice => "Please insert email and enable notifcation in your global setting."
+      redirect_to new_setting_path, :notice => "you need to set email and enable notification to test global setting."
     end
   end
 end
